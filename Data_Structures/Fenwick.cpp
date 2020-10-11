01544408a436444ee4e1203a321431d220294a77
joshi1411 <17475@nith.ac.in>
2020-10-11 22:09:16 +0530

    fenwick

--- Data_Structures/Fenwick.cpp
+++ Data_Structures/Fenwick.cpp
@@ -28,3 +28,39 @@ struct Fenwick {
             bit[idx] += delta;
     }
 };
+
+
+//1 based indexing
+struct Fenwick1 {
+    vector<int> bit;  // binary indexed tree
+    int n;
+
+    Fenwick1(int n) {
+        this->n = n + 1;
+        bit.assign(n + 1, 0);
+    }
+
+    Fenwick1(vector<int> a)
+        : Fenwick1(a.size()) {
+        for (size_t i = 0; i < a.size(); i++)
+            add(i, a[i]);
+    }
+
+    int sum(int idx) {
+        int ret = 0;
+        for (++idx; idx > 0; idx -= idx & -idx)
+            ret += bit[idx];
+        return ret;
+    }
+
+    int sum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
+
+    void add(int idx, int delta) {
+        for (++idx; idx < n; idx += idx & -idx)
+            bit[idx] += delta;
+    }
+};
+
+
